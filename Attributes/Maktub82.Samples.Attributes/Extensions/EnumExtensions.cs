444fe2401e07092512015c3ddf9cf52245fc2eec
maktub82 <gallardo91_@hotmail.com>
2016-03-22 22:07:13 +0100

    Added Attribute Samples Added DisplayName Attributes
    Added all attribute examples
    
    Added multiple attributes

---
+++ Attributes/Maktub82.Samples.Attributes/Extensions/EnumExtensions.cs
@@ -0,0 +1,61 @@
+﻿using Maktub82.Samples.Attributes.Attributes;
+using Maktub82.Samples.Attributes.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Maktub82.Samples.Attributes.Extensions
+{
+    public static class EnumExtensions
+    {
+        #region Century Example
+        public static int GetStartYear(this Enum enumValue)
+        {
+            var attribute = GetFirstOrDefaultAttribute<CenturyDataAttribute>(enumValue);
+            return attribute != null ? attribute.StartYear : 0;
+        }
+
+        public static int GetEndYear(this Enum enumValue)
+        {
+            var attribute = GetFirstOrDefaultAttribute<CenturyDataAttribute>(enumValue);
+            return attribute != null ? attribute.EndYear : 0;
+        }
+
+        public static string GetCenturyName(this Enum enumValue)
+        {
+            var attribute = GetFirstOrDefaultAttribute<CenturyDataAttribute>(enumValue);
+            return attribute != null ? attribute.DisplayName : string.Empty;
+        }
+        #endregion
+
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            var attribute = GetFirstOrDefaultAttribute<DisplayNameAttribute>(enumValue);
+            return attribute != null ? attribute.DisplayName : string.Empty;
+        }
+
+        public static string GenerateQuery(this Enum enumValue)
+        {
+            var attributes = GetAttributes<ApiValueAttribute>(enumValue);
+            IEnumerable<string> values = attributes.Select(attribute => attribute.ApiValue);
+            return $"{string.Join(",", values)}";
+        }
+
+        #region Private Fields
+        private static T GetFirstOrDefaultAttribute<T>(Enum enumValue) where T : Attribute
+        {
+            var attributes = GetAttributes<T>(enumValue);
+            return attributes.FirstOrDefault() as T;
+        }
+
+        private static IEnumerable<T> GetAttributes<T>(Enum enumValue) where T : Attribute
+        {
+            var type = enumValue.GetType();
+            var memberInfo = type.GetMember(enumValue.ToString());
+            var attributes = memberInfo[0].GetCustomAttributes(typeof(T), false);
+            return attributes.Cast<T>();
+        }
+        #endregion
+
+    }
+}
---
+++ Attributes/Maktub82.Samples.Attributes/Program.cs
@@ -0,0 +1,36 @@
+﻿using Maktub82.Samples.Attributes.Enums;
+using Maktub82.Samples.Attributes.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maktub82.Samples.Attributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Show Fruits Name:");
+            Console.WriteLine(Fruit.BloodOrange.GetDisplayName());
+            Console.WriteLine(Fruit.Watermelon.GetDisplayName());
+            Console.WriteLine(Fruit.Lemon.GetDisplayName());
+            Console.WriteLine();
+
+            Console.WriteLine("Show Centuries:");
+            Console.WriteLine($"Century {Century.XV.GetCenturyName()} [{Century.XV.GetStartYear()},{Century.XV.GetEndYear()}]");
+            Console.WriteLine($"Century {Century.XX.GetCenturyName()} [{Century.XX.GetStartYear()},{Century.XX.GetEndYear()}]");
+            Console.WriteLine($"Century {Century.XVIII.GetCenturyName()} [{Century.XVIII.GetStartYear()},{Century.XVIII.GetEndYear()}]");
+            Console.WriteLine();
+
+            Console.WriteLine("Real Example. Attribute to filter in Api search");
+            Console.WriteLine($"To Search by {Category.Films.GetDisplayName()} category: \nhttp://example.api.fake?query=the&category={Category.Films.GenerateQuery()}\n");
+            Console.WriteLine($"To Search by {Category.Series.GetDisplayName()} category: \nhttp://example.api.fake?query=the&category={Category.Series.GenerateQuery()}\n");
+            Console.WriteLine($"To Search by {Category.Documentary.GetDisplayName()} category: \nhttp://example.api.fake?query=the&category={Category.Documentary.GenerateQuery()}\n");
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
