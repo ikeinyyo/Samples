aed65a76b048e4e7d0ed2194680642824a670526
maktub82 <gallardo91_@hotmail.com>
2016-07-07 11:12:34 +0200

    Add Cortana Integration
    Fix Cortana integration

---
+++ Speech/Maktub82.Sample.Speech/TextCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace Maktub82.Sample.Speech
+{
+    public enum TextCommandType
+    {
+        Upper,
+        Lower,
+        Reverse
+    }
+
+    public class TextCommand
+    {
+        public TextCommandType Type { get; set; }
+        public string Sentence { get; set; }
+    }
+}
